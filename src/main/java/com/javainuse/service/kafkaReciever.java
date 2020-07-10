7783e17f0e0523360309a5ca2c36fe24678e8f6a
Himanshurock <himanshurock85@gmail.com>
2020-07-11 01:03:44 +0530

    Update kafkaReciever.java

--- src/main/java/com/javainuse/service/kafkaReciever.java
+++ src/main/java/com/javainuse/service/kafkaReciever.java
@@ -1,5 +1,7 @@
 package com.javainuse.service;
 
+import java.util.concurrent.CountDownLatch;
+
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
@@ -7,11 +9,17 @@ import org.springframework.stereotype.Component;
 @Component
 public class kafkaReciever {
 	
+	 private CountDownLatch latch = new CountDownLatch(1);
+
+	  public CountDownLatch getLatch() {
+	    return latch;
+	  }
 	
 	 @StreamListener("InputCommonChannelTest")
 	public void reciever(@Payload String msg) {
 		 
 		 System.out.println("============msg======="+msg);
+		 latch.countDown();
 	}
 	 
 	 
