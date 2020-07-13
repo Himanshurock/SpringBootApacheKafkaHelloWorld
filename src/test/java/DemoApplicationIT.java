629b24ed27dc0ad77250c534c76113e76ccfe56a
Himanshurock <himanshurock85@gmail.com>
2020-07-13 16:21:11 +0530

    Update DemoApplicationIT.java

--- src/test/java/DemoApplicationIT.java
+++ src/test/java/DemoApplicationIT.java
@@ -1,5 +1,4 @@
 
-import static org.mockito.ArgumentMatchers.argThat;
 import static org.mockito.Mockito.timeout;
 import static org.mockito.Mockito.verify;
 
@@ -15,15 +14,19 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.context.annotation.Bean;
-import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.javainuse.SpringBootHelloWorldApplication;
+import com.javainuse.service.KafkaSender;
 import com.javainuse.service.MessageBinding;
 import com.javainuse.service.kafkaReciever;
+
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
+import static org.mockito.ArgumentMatchers.argThat;
 
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -36,7 +39,7 @@ import com.javainuse.service.kafkaReciever;
         properties = {
         		"spring.cloud.stream.bindings.InputCommonChannelTest.destination=MyTopic",
         		 "spring.cloud.stream.bindings.CommonChannelTest.destination=MyTopic",
-        		 "spring.kafka.bootstrap-servers=kafka:9092"
+        		 "spring.kafka.bootstrap-servers=localhost:9092"
         })
 public class DemoApplicationIT {
 	
@@ -70,18 +73,23 @@ public class DemoApplicationIT {
 	    private kafkaReciever listener;
 
 	    @Autowired
+	    private KafkaSender producer;
+	    
+	    @Autowired
 		  private MessageBinding channel;
 	    
 	    @Test
 	    public void messageIsReceived() {
-	        MessageRequest req = new MessageRequest("abc12344");
-	        System.out.println("Before sent========");
+	        MessageRequest req = new MessageRequest("abc123");
+	        System.out.println("Before==========");
 	        channel.messageChannel().send(MessageBuilder.withPayload(req).build());
-	        System.out.println("After sent========");
+	        System.out.println("After==========");
+
 	        // the message actually gets received. Need to do a timeout because I cannot manually force
 	        // a consumption of this message from Kafka. The default for timeout() is to check every
 	        // 10ms up to the timeout
 	        verify(listener, timeout(5000)).reciever(argThat(m -> m.getId().equals(req.getId())));
+	       // reciever(argThat(m -> m..equals(req.getId())));
 
 	    }
 	    public static class MessageRequest {
